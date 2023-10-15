68890f3dfd9c1e4aeadb1e2f38df079f41c6d250
ethanminglee <112620722+ethanminglee@users.noreply.github.com>
2023-10-15 23:51:26 +1100

    added city parking vis

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -7,3 +7,8 @@ var vg_2 = "js/transport.vg.json";
 vegaEmbed("#transport", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "js/parking_times.vg.json";
+vegaEmbed("#parking", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
