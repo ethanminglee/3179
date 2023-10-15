5e08e99644949888107fc45903eea0a2a7025998
ethanminglee <112620722+ethanminglee@users.noreply.github.com>
2023-10-15 20:06:34 +1100

    fixed refs

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,9 +1,9 @@
-var vg_1 = "/js/melbourne_map.vg.json";
+var vg_1 = "js/melbourne_map.vg.json";
 vegaEmbed("#melbourne_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "/js/transport.vg.json";
+var vg_2 = "js/transport.vg.json";
 vegaEmbed("#transport", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
