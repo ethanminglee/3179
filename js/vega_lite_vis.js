bd89aecb581dd78921edc3ce518c15df7d9fb70c
ethanminglee <ethan.mhlee@gmail.com>
2023-10-17 14:13:01 +1100

    updated styling of page

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,14 +1,14 @@
 var vg_1 = "js/melbourne_map.vg.json";
-vegaEmbed("#melbourne_map", vg_1).then(function(result) {
+vegaEmbed("#melbourne_map", vg_1, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "js/transport.vg.json";
-vegaEmbed("#transport", vg_2).then(function(result) {
+vegaEmbed("#transport", vg_2, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "js/parking_times.vg.json";
-vegaEmbed("#parking", vg_3).then(function(result) {
+vegaEmbed("#parking", vg_3, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
